61dac8f610504d0268109fd17748e9c89a25cdf2
TheDoubleRainbow <16mb.com@gmail.com>
2017-10-07 23:42:14 +0300

    Vue

--- js/player.vue
+++ js/player.vue
@@ -1,3 +1,10 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
+var Vue = require('./vue.js');
+new Vue({
+	el: "#main",
+	data: {
+		tracks: ["New Pirate.mp3, Russian rock.mp3, Lolita.mp3"],
+		currentTrack: {name: "New Pirate.mp3", pic: "../pic/track.jpg"},
+	},
+	create: function(){
+	}
+})
