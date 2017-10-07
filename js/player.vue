af533a8cbcd07f7327ac0462c95cba2cf2284568
TheDoubleRainbow <16mb.com@gmail.com>
2017-10-08 01:11:27 +0300

    play button

--- js/player.vue
+++ js/player.vue
@@ -4,7 +4,21 @@ new Vue({
 	data: {
 		tracks: ["New Pirate.mp3, Russian rock.mp3, Lolita.mp3"],
 		currentTrack: {name: "New Pirate.mp3", pic: "../pic/track.jpg"},
+		player: {
+			html: document.querySelector('#player'),
+			state: false,
+		}
 	},
-	create: function(){
+	methods: {
+		play: function(){
+			if(this.player.state){
+				this.player.html.pause();
+				this.player.state = false;
+			}
+			else{
+				this.player.html.play()
+				this.player.state = true;
+			}
+		}
 	}
 })
