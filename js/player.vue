0f0115fb62e668aaf9184f8d255502a854956cd2
TheDoubleRainbow <16mb.com@gmail.com>
2017-10-08 05:42:44 +0300

    glob + id3

--- js/player.vue
+++ js/player.vue
@@ -23,6 +23,9 @@ new Vue({
 			let that = this;
 			this.tracks = [];
 			glob("music/*.mp3", function (er, files) {
+				id3({ file: files[0], type: id3.OPEN_LOCAL }, function(err, tags) {
+    					that.currentTrack = {name: tags.artist + ' - ' + tags.title, genre: tags.v1.genre, way: files[0]};
+				});
 				files.forEach(function(file){
 					id3({ file: file, type: id3.OPEN_LOCAL }, function(err, tags) {
     					that.tracks.push({name: tags.artist + ' - ' + tags.title, genre: tags.v1.genre, way: file})
