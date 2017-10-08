d214cf94edd29a5347def087cfd303199421644d
TheDoubleRainbow <16mb.com@gmail.com>
2017-10-08 05:37:42 +0300

    id3

--- js/player.vue
+++ js/player.vue
@@ -1,15 +1,35 @@
 var Vue = require('./vue.js');
+var glob = require('glob');
+var id3 = require('id3js');
+
 new Vue({
 	el: "#main",
 	data: {
-		tracks: ["New Pirate.mp3, Russian rock.mp3, Lolita.mp3"],
-		currentTrack: {name: "Suicide Is Painless", pic: "../pic/track.jpg"},
+		tracks: [],
+		currentTrack: {},
 		player: {
 			html: document.querySelector('#player'),
 			state: false,
 		}
 	},
+	created: function(){
+		this.getTracks(true)
+	},
 	methods: {
+		setTrack: function(id){
+			console.log(this.tracks)
+		},
+		getTracks: function(){
+			let that = this;
+			this.tracks = [];
+			glob("music/*.mp3", function (er, files) {
+				files.forEach(function(file){
+					id3({ file: file, type: id3.OPEN_LOCAL }, function(err, tags) {
+    					that.tracks.push({name: tags.artist + ' - ' + tags.title, genre: tags.v1.genre, way: file})
+					});
+				})
+			})
+		},
 		play: function(){
 			if(this.player.state){
 				this.player.html.pause();
