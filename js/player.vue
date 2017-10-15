4c4bbdb727fdd90735bc69e4187cddfb8a09024c
TheDoubleRainbow <16mb.com@gmail.com>
2017-10-15 23:23:59 +0300

    next finished

--- js/player.vue
+++ js/player.vue
@@ -3,39 +3,48 @@ var glob = require('glob');
 var id3 = require('id3js');
 
 new Vue({
-	el: "#main",
-	data: {
-		tracks: [],
-		currentTrack: {},
-		player: {
-			volume: 1
-		}
-	},
-	created: function(){
-		this.getTracks()
-	},
-	methods: {
-		setTrack: function(track){
-			this.currentTrack = track;
-			setTimeout(function(){document.getElementById("music").play()}, 1000)
-			music.paused = false;
-		},
-		getTracks: function(){
-			let that = this;
-			this.tracks = [];
-			glob("music/*.mp3", function (er, files) { //Searching for files
-				id3({ file: files[0], type: id3.OPEN_LOCAL }, function(err, tags) { // getting id3 tags from mp3
-    					that.currentTrack = {name: tags.artist + ' - ' + tags.title, genre: tags.v1.genre, way: files[0]};
-
-				});
-				files.forEach(function(file){
-					id3({ file: file, type: id3.OPEN_LOCAL }, function(err, tags) {
-    					that.tracks.push({name: (tags.artist + ' - ' + tags.title), genre: tags.v1.genre, way: file})
-					});
-				})
-			})
-		},
-	}
+    el: "#main",
+    data: {
+        tracks: [],
+        currentTrack: {},
+        player: {
+            volume: 1
+        }
+    },
+    created: function() {
+        this.getTracks()
+    },
+    methods: {
+        setTrack: function(track) {
+            if (track == "next") {
+                if (this.tracks[this.tracks.indexOf(this.currentTrack) + 1]) {
+                	console.log(this.tracks[this.tracks.indexOf(this.currentTrack) + 1])
+                    this.currentTrack = this.tracks[this.tracks.indexOf(this.currentTrack) + 1];
+                    setTimeout(function() { document.getElementById("music").play() }, 1000)
+                } else {
+                    console.log("Next track does not exist")
+                }
+            } else {
+                this.currentTrack = track;
+                setTimeout(function() { document.getElementById("music").play() }, 1000)
+                music.paused = false;
+            }
+        },
+        getTracks: function() {
+            let that = this;
+            this.tracks = [];
+            glob("music/*.mp3", function(er, files) {
+                files.forEach(function(file) {
+                    id3({ file: file, type: id3.OPEN_LOCAL }, function(err, tags) {
+                        that.tracks.push({ name: (tags.artist + ' - ' + tags.title), genre: tags.v1.genre, way: file })
+                    });
+                })
+                setTimeout(function(){
+                	that.currentTrack = that.tracks[0]
+                }, 500)
+            })
+        },
+    }
 })
 
 require('./audioControls.js'); //player controls
